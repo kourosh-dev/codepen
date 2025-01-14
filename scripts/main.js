3628faaf4bf5c7e215470b83f730dd1937eae07f
kourosh <kourosh0903@gmail.com>
2025-01-14 11:16:42 -0800

    added js classes

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('.js-header');
+const codepenIcon = document.querySelector('.js-icon-codepen');
+const adsImg = document.querySelector('.js-ads');
+const btnOpenMenu = document.querySelector('.js-button-open-menu');
+const graphicElm = document.querySelector('.js-graphic-btn');
