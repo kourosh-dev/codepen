603910885b459f21cae65b8976e3794fe30866e3
kourosh <kourosh0903@gmail.com>
2025-01-22 09:43:13 -0800

    made tooltip button responsive for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,17 @@ const codepenIcon = document.querySelector('.js-icon-codepen');
 const adsImg = document.querySelector('.js-ads');
 const btnOpenMenu = document.querySelector('.js-button-open-menu');
 const graphicElm = document.querySelector('.js-graphic-btn');
+const arrow = document.querySelector('.js-arrow-rotate');
+
+btnOpenMenu.addEventListener('click', () => {
+  if (header.style.display === 'block') {
+    header.style.display = 'none';
+    arrow.style.transform = 'rotate(0)';
+    graphicElm.classList.remove('opened-tooltip');
+  } else {
+    arrow.style.transform = 'rotate(180deg)';
+    header.style.display = 'block';
+    graphicElm.classList.add('opened-tooltip');
+  }
+});
+
